7640f3ed3ddc47f06f1ee3ee4ddb97a53644a1f0
Jon Wenger <jonwenger783@gmail.com>
2018-06-22 20:25:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,13 +20,29 @@ def self.get_count_other_way
     @@count
 end 
 
-
 end
 
 jon_song = Song.new("Hippity Hoppity", "Jon Mendez", "Jon's Way")
 big_song = Song.new("l", "n", "u")
 
+puts "this is the first method"
 puts jon_song.get_count
+puts "this is the second method"
+puts Song.get_count_other_way
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
